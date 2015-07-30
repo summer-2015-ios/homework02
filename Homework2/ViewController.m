90cf55e13c0c5500c064fdb4b2e1f1e55ac2432e
student <student@ccibio104-206.uncc.edu>
2015-07-30 10:14:53 -0400

    Initial Commit

---
+++ Homework2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Homework2
+//
+//  Created by student on 7/30/15.
+//  Copyright (c) 2015 student. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
